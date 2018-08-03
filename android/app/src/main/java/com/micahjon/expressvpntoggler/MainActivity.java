7b55b03bc9b29fa21ca5b94abc5357e692feb817
Micah Miller-Eshleman <micah.millereshleman@gmail.com>
2018-08-02 22:05:07 -0700

    Use device's MAC address (instead of mock). Rebuild APK.

--- android/app/src/main/java/com/micahjon/expressvpntoggler/MainActivity.java
+++ android/app/src/main/java/com/micahjon/expressvpntoggler/MainActivity.java
@@ -53,34 +53,34 @@ public class MainActivity extends FlutterActivity {
     public static String getMacAddress() {
 
         // TEMP - FOR DEBUGGING
-        String str = "a4:5e:60:e1:c0:11";
-        return str.toUpperCase();
+//        String str = "a4:5e:60:e1:c0:11";
+//        return str.toUpperCase();
 
         //--
-//
-//        try {
-//            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
-//            for (NetworkInterface nif : all) {
-//                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
-//
-//                byte[] macBytes = nif.getHardwareAddress();
-//                if (macBytes == null) {
-//                    return "";
-//                }
-//
-//                StringBuilder res1 = new StringBuilder();
-//                for (byte b : macBytes) {
-//                    res1.append(Integer.toHexString(b & 0xFF) + ":");
-//                }
-//
-//                if (res1.length() > 0) {
-//                    res1.deleteCharAt(res1.length() - 1);
-//                }
-//                return res1.toString();
-//            }
-//        } catch (Exception ex) {
-//            //handle exception
-//        }
-//        return "";
+
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(Integer.toHexString(b & 0xFF) + ":");
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+            //handle exception
+        }
+        return "";
     }
 }
